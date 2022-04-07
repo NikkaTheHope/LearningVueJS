124735dd12763d5ec3bed677c02efa1452ffefcf
nikkathehope <nikkathehope@gmail.com>
2022-04-07 11:07:07 +0900

    Data Binding

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-#Start Learning About Vue.js
+# Start Learning About Vue.js
 ### npm install -g @vue/cli
     - Install Vue.js
 ### Used Extensions
-    - HTML, CSS, Supporter
+    - HTML, CSS Support
     - Vue 3 snippets
-    - 
+    - Vetur
+
+## Data Binding In Vue.js
+### {{}} Why we use this grammar
+    - 변경용이성 changable
+    - 실시간 렌더링 Realtime Rendering
+    - When data changes, html renders
+    - :attr="name" => databinding
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    {{logo}}
+    <h4 :style="스타일">{{products[0]}}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data(){
+    return {
+      price1: 60,
+      price2: 70,
+      logo: `원룸샵`,
+      스타일: `color: blue;`,
+      products : ["역삼동원룸","천호동원룸","마포구원룸"],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
